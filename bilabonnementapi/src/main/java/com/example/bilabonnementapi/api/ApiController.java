3a73f1b1ab262390565e0b1728a30324fa0cc4b5
Fredvigsoe <112333260+Fredvigsoe@users.noreply.github.com>
2023-12-07 15:19:58 +0100

    Update ApiController.java

--- bilabonnementapi/src/main/java/com/example/bilabonnementapi/api/ApiController.java
+++ bilabonnementapi/src/main/java/com/example/bilabonnementapi/api/ApiController.java
@@ -18,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ApiController {
 
@@ -32,6 +31,7 @@ public class ApiController {
     private SubscriptionService subscriptionService;
 
     @GetMapping("/")
+    @CrossOrigin(origins = "*")
     public String getAllEndpoints() {
         return "<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
@@ -211,27 +211,32 @@ public class ApiController {
     //Carcontrollers
 
     @GetMapping("/cars")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<List<Car>> getAllCars() {
         List<Car> cars = carService.getAllCars();
         return ResponseEntity.ok().body(cars);
     }
 
     @GetMapping("/cars/{id}")
+    @CrossOrigin(origins = "*")
     public Car getCarById(@PathVariable String id) {
         return carService.getCarById(id);
     }
     @PostMapping("/cars")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Car> createCar(@RequestBody Car car) {
         Car newCar = carService.createCar(car);
         return new ResponseEntity<>(newCar, HttpStatus.CREATED);
     }
 
     @PutMapping("/cars/{id}")
+    @CrossOrigin(origins = "*")
     public Car updateCar(@PathVariable String id, @RequestBody Car car) {
         return carService.updateCar(id, car);
     }
 
     @DeleteMapping("/cars/{id}")
+    @CrossOrigin(origins = "*")
     public void deleteCar(@PathVariable String id) {
         carService.deleteCar(id);
     }
@@ -239,60 +244,70 @@ public class ApiController {
     //Customer controllers
 
     @GetMapping("/customers")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<List<Customer>> getAllCustomers() {
         List<Customer> customers = customerService.getAllCustomers();
         return ResponseEntity.ok().body(customers);
     }
 
     @GetMapping("/customers/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Customer> getCustomerById(@PathVariable String id) {
         Customer customer = customerService.getCustomerById(id);
         return ResponseEntity.ok().body(customer);
     }
 
     @PostMapping("/customers")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
         Customer newCustomer = customerService.createCustomer(customer);
         return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
     }
 
     @PutMapping("/customers/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Customer> updateCustomer(@PathVariable String id, @RequestBody Customer customer) {
         Customer updatedCustomer = customerService.updateCustomer(id, customer);
         return ResponseEntity.ok().body(updatedCustomer);
     }
 
     @DeleteMapping("/customers/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Void> deleteCustomer(@PathVariable String id) {
         customerService.deleteCustomer(id);
         return ResponseEntity.noContent().build();
     }
     // Damage Report Controllers
     @GetMapping("/damagereports")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<List<DamageReport>> getAllDamageReports() {
         List<DamageReport> damageReports = damageReportService.getAllDamageReports();
         return ResponseEntity.ok().body(damageReports);
     }
 
     @GetMapping("/damagereports/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<DamageReport> getDamageReportById(@PathVariable String id) {
         DamageReport damageReport = damageReportService.getDamageReportById(id);
         return ResponseEntity.ok().body(damageReport);
     }
 
     @PostMapping("/damagereports")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<DamageReport> createDamageReport(@RequestBody DamageReport damageReport) {
         DamageReport newDamageReport = damageReportService.createDamageReport(damageReport);
         return new ResponseEntity<>(newDamageReport, HttpStatus.CREATED);
     }
 
     @PutMapping("/damagereports/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<DamageReport> updateDamageReport(@PathVariable String id, @RequestBody DamageReport damageReport) {
         DamageReport updatedDamageReport = damageReportService.updateDamageReport(id, damageReport);
         return ResponseEntity.ok().body(updatedDamageReport);
     }
 
     @DeleteMapping("/damagereports/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Void> deleteDamageReport(@PathVariable String id) {
         damageReportService.deleteDamageReport(id);
         return ResponseEntity.noContent().build();
@@ -301,30 +316,35 @@ public class ApiController {
 
     // Subscription Controllers
     @GetMapping("/subscriptions")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<List<Subscription>> getAllSubscriptions() {
         List<Subscription> subscriptions = subscriptionService.getAllSubscriptions();
         return ResponseEntity.ok().body(subscriptions);
     }
 
     @GetMapping("/subscriptions/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Subscription> getSubscriptionById(@PathVariable String id) {
         Subscription subscription = subscriptionService.getSubscriptionById(id);
         return ResponseEntity.ok().body(subscription);
     }
 
     @PostMapping("/subscriptions")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Subscription> createSubscription(@RequestBody Subscription subscription) {
         Subscription newSubscription = subscriptionService.createSubscription(subscription);
         return new ResponseEntity<>(newSubscription, HttpStatus.CREATED);
     }
 
     @PutMapping("/subscriptions/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Subscription> updateSubscription(@PathVariable String id, @RequestBody Subscription subscription) {
         Subscription updatedSubscription = subscriptionService.updateSubscription(id, subscription);
         return ResponseEntity.ok().body(updatedSubscription);
     }
 
     @DeleteMapping("/subscriptions/{id}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Void> deleteSubscription(@PathVariable String id) {
         subscriptionService.deleteSubscription(id);
         return ResponseEntity.noContent().build();
